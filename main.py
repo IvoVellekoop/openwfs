410043f905eb22d618127aaf6cd611d5d63521ba
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2023-06-25 21:49:05 +0200

    updated test to include new features

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from patch import Patch
 import numpy as np
 from math import pi
 import geometry
+import textures
 
 # construct a new SLM object and add a patch to it
 numerical_aperture = 0.8
@@ -11,6 +12,7 @@ s2 = SLM(0, left=500)
 g = geometry.square(numerical_aperture)
 g[1, 1, 1] = 0
 p1 = Patch(s1, g)
+pf = Patch(s1, geometry.square(1.0))
 p2 = Patch(s2, geometry.square(1.0))
 p3 = Patch(s2, geometry.square(0.2))
 p4 = Patch(s2, geometry.square(0.1))
@@ -18,12 +20,13 @@ p3.phases = 0.25
 p4.phases = 1
 p4.additive_blend = False
 
+pf.phases = textures.defocus(100) * 6
+p1.enabled = False
 rng = np.random.default_rng()
 for n in range(50):
     data = rng.random([10, 10], np.float32) * 2.0 * pi
     p1.phases = data
     p2.phases = n/4.0
-    print(n/4.0)
     s1.update()
     s2.update()
 
