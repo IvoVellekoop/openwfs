0e22f58be448ca76373e961ac216b32809021447
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2023-07-14 11:06:30 +0200

    bug fix in computing enhancement (missing update)

--- tests/wfs_test.py
+++ tests/wfs_test.py
@@ -8,28 +8,32 @@ from algorithms import StepwiseSequential2
 from feedback import SimpleCameraFeedback
 
 """This """
-def calculate_enhancement(simulation,wfs_experiment):
+
+
+def calculate_enhancement(simulation, wfs_experiment):
     simulation.set_data(0)
+    simulation.update()
     simulation.trigger()
     simulation.wait()
-    feedback_before = sum(simulation.get_image()[250:251,250:251])
+    feedback_before = np.mean(simulation.get_image()[250:251, 250:251])
 
     simulation.set_data(wfs_experiment.optimised_wf)
+    simulation.update()
     simulation.trigger()
     simulation.wait()
-    feedback_after = sum(simulation.get_image()[250:251,250:251])
+    feedback_after = np.mean(simulation.get_image()[250:251, 250:251])
 
-    return feedback_after/feedback_before
+    return feedback_after / feedback_before
+
 
 def flat_wf_response_fourier():
-
     wfs = WfsExperiment()
     sim = SimulatedWFS()
-    sim.set_ideal_wf(np.zeros([500, 500])) # correct wf = flat
-    sim.E_input_slm = np.ones([500, 500]) # set image plane size, and gauss off
+    sim.set_ideal_wf(np.zeros([500, 500]))  # correct wf = flat
+    sim.E_input_slm = np.ones([500, 500])  # set image plane size, and gauss off
 
     wfs.algorithm = FourierDualRef()
-    wfs.ranges = [[250, 251], [250, 251]] # get feedback from the center of the image plane
+    wfs.ranges = [[250, 251], [250, 251]]  # get feedback from the center of the image plane
     wfs.algorithm.ky_angles_max = 1
     wfs.algorithm.ky_angles_min = -1
     wfs.algorithm.kx_angles_max = 1
@@ -49,14 +53,13 @@ def flat_wf_response_fourier():
 
 
 def flat_wf_response_ssa():
-
     wfs = WfsExperiment()
     sim = SimulatedWFS()
-    sim.set_ideal_wf(np.zeros([500, 500])) # correct wf = flat
-    sim.E_input_slm = np.ones([500, 500]) # set image plane size, and gauss off
+    sim.set_ideal_wf(np.zeros([500, 500]))  # correct wf = flat
+    sim.E_input_slm = np.ones([500, 500])  # set image plane size, and gauss off
 
     wfs.algorithm = StepwiseSequential()
-    wfs.ranges = [[250, 251], [250, 251]] # get feedback from the center of the image plane
+    wfs.ranges = [[250, 251], [250, 251]]  # get feedback from the center of the image plane
     wfs.algorithm.n_slm_fields = 3
 
     wfs.slm_object = sim
@@ -68,10 +71,11 @@ def flat_wf_response_ssa():
     else:
         return True
 
+
 def flat_wf_response_ssa2():
     sim = SimulatedWFS()
-    sim.set_ideal_wf(np.zeros([500, 500])) # correct wf = flat
-    sim.E_input_slm = np.ones([500, 500]) # set image plane size, and gauss off
+    sim.set_ideal_wf(np.zeros([500, 500]))  # correct wf = flat
+    sim.E_input_slm = np.ones([500, 500])  # set image plane size, and gauss off
 
     f = SimpleCameraFeedback(camera=sim, slm=sim, roi_x=250, roi_y=250, roi_radius=1)
     alg = StepwiseSequential2(Nx=8, Ny=4, feedback=f, slm=sim)
@@ -84,15 +88,15 @@ def flat_wf_response_ssa2():
     else:
         return True
 
-def wf_response_fourier():
 
+def wf_response_fourier():
     wfs = WfsExperiment()
     sim = SimulatedWFS()
-    sim.set_ideal_wf(data.camera()) # correct wf = flat
-    sim.E_input_slm = np.ones([500, 500]) # set image plane size, and gauss off
+    sim.set_ideal_wf(data.camera())  # correct wf = flat
+    sim.E_input_slm = np.ones([500, 500])  # set image plane size, and gauss off
 
     wfs.algorithm = FourierDualRef()
-    wfs.ranges = [[250, 251], [250, 251]] # get feedback from the center of the image plane
+    wfs.ranges = [[250, 251], [250, 251]]  # get feedback from the center of the image plane
     wfs.algorithm.ky_angles_max = 1
     wfs.algorithm.ky_angles_min = -1
     wfs.algorithm.kx_angles_max = 1
@@ -106,8 +110,9 @@ def wf_response_fourier():
 
     wfs.execute = 1
 
-    if calculate_enhancement(sim,wfs) < 3:
-        raise Exception("Fourier algorithm does not enhance focus as much as expected")
+    enhancement = calculate_enhancement(sim, wfs)
+    if enhancement < 3:
+        raise Exception(f"Fourier algorithm does not enhance focus as much as expected. Expected 3, got {enhancement}")
     else:
         return True
 
