fixed microscope scaling

examples/sample_microscope.py
@@ -5,13 +5,13 @@ from openwfs.simulation import Microscope, MockImageSource
 
 ### Parameters that can be altered
 
-img_size_x = 500
+img_size_x = 1024
 # Determines how wide the image is.
 
-img_size_y = 500
+img_size_y = 1024
 # Determines how high the image is.
 
-magnification = 10
+magnification = 40
 # magnification from object plane to camera.
 
 numerical_aperture = 0.85
@@ -31,7 +31,7 @@ p_limit = 100
 
 ## Code
 img = np.maximum(np.random.randint(-10000, 100, (img_size_y, img_size_x), dtype=np.int16), 0)
-src = MockImageSource.from_image(img, 100 * u.nm)
+src = MockImageSource.from_image(img, 50 * u.nm)
 mic = Microscope(src, magnification=magnification, numerical_aperture=numerical_aperture, wavelength=wavelength,
                  camera_pixel_size=pixel_size, camera_resolution=camera_resolution)
 mic.camera.saturation = 70.0
@@ -47,10 +47,13 @@ if __name__ == '__main__':
     ax = plt.subplot(1, 2, 2)
     plt.title('Scanned image')
     for p in range(p_limit):
-        mic.xy_stage.x = p * 1 * u.um
-        mic.numerical_aperture = 1.0  # * (p + 1) / p_limit  # NA increases to 1.0
+        # mic.xy_stage.x = p * 1 * u.um
+        mic.numerical_aperture = 1.0 * (p + 1) / p_limit  # NA increases to 1.0
         c.trigger()
         cim = c.read()
-        ax.imshow(cim)
+        plt.imshow(cim, extent=c.extent().to_value(u.um) / magnification, cmap='gray')
+        plt.xlabel('μm')
+        plt.ylabel('μm')
+        plt.title(f"NA: {mic.numerical_aperture}, δ: {mic.abbe_limit.to_value(u.um):2.2} μm")
         plt.draw()
         plt.pause(0.2)

openwfs/simulation/microscope.py
@@ -86,11 +86,12 @@ class Microscope:
         # post-processors may be added to simulated physical effects like noise, bias, and saturation.
         self.camera = MockImageSource(data_shape=camera_resolution,
                                       pixel_size=camera_pixel_size,
-                                      on_trigger=lambda i: self._update(i))
+                                      on_trigger=lambda: self._update())
+        self.abbe_limit = 0.0
         self._pupil_resolution = 0.0
         self.psf = None
 
-    def _update(self, image):
+    def _update(self):
         """Updates the image on the camera sensor
 
         To compute the image:
@@ -123,8 +124,9 @@ class Microscope:
         # a Nyquist limit of λ/(2.0·NA), which is the Abbe diffraction limit.
         # The total field of view holds 2.0 Δ NA / λ diffraction limited points,
         # which means that we need exactly that many points in the NA.
-        self._pupil_resolution = np.ceil(
-            (fov * self.numerical_aperture / self.wavelength).to_value(u.dimensionless_unscaled))
+        self.abbe_limit = 0.5 * self.wavelength / self.numerical_aperture
+        self._pupil_resolution = int(np.ceil(float(fov / self.abbe_limit)))
+        print(self._pupil_resolution)
         pupil_field = patterns.disk(self._pupil_resolution)
         if self.aberrations is not None and self.slm is not None:
             pupil_field *= np.exp(1.0j * (self._read_crop(self.aberrations) + self._read_crop(self.slm)))
@@ -135,11 +137,12 @@ class Microscope:
 
         # finally, pad the pupil field so that the diameter of the pupil field
         # corresponds to a focus with the size of a single pixel in the source image
-        # invert the diffraction limit: 'NA_pad' = λ / (2 δ)
-        # compute how much larger this is than the actual NA: λ / (2 δ) / NA
-        # compute total number of points after padding
-        pupil_resolution = round((0.5 * self.wavelength / self.source.pixel_size / self.numerical_aperture).to_value(
-            u.dimensionless_unscaled) * self._pupil_resolution)
+        # - first compute the ratio of Abbe limit (i.e. the resolution corresponding to the current pupil size)
+        #   to the pixel size of the source image (i.e. the resolution corresponding to the padded pupil size)
+        # - then pad the pupil field so that the size gets multiplied by that ratio
+        pupil_resolution = round(float(self.abbe_limit / self.source.pixel_size) * self._pupil_resolution)
+        if pupil_resolution < self._pupil_resolution:
+            raise Exception("Pixel size in the source image is larger than the Abbe diffraction limit")
 
         # compute the PSF
         psf = np.abs(np.fft.fft2(np.fft.ifftshift(pupil_field), (pupil_resolution, pupil_resolution))) ** 2
@@ -157,8 +160,7 @@ class Microscope:
         offset[1, 2] += self.xy_stage.y / self.source.pixel_size
         m = m @ offset  # apply offset first, then magnification
 
-        affine_transform(s, m, output=image, order=1)
-        return image
+        return affine_transform(s, np.linalg.inv(m), order=1, output_shape=self.camera.data_shape)
 
     def _read_crop(self, source):
         """crop/pad an image to the NA of the microscope objective and scale to the internal resolution"""

openwfs/simulation/mockdevices.py
@@ -10,7 +10,7 @@ class MockImageSource:
 
     def __init__(self, on_trigger, data_shape, pixel_size: Quantity[u.um]):
         self._on_trigger = on_trigger
-        self._image = np.empty(data_shape, dtype="float32")
+        self._image = None
         self._measurement_time = 0  # property may be set to something else to mimic acquisition time
         self._triggered = 0  # currently only support a buffer with a single frame
         self.pixel_size = pixel_size.to(u.um)
@@ -21,7 +21,7 @@ class MockImageSource:
         if self._triggered != 0:
             raise RuntimeError('Buffer overflow: this camera can only store 1 frame, and the previous frame was not '
                                'read yet')
-        self._image = self._on_trigger(self._image)
+        self._image = self._on_trigger()
         self._triggered += 1
 
     def read(self):
@@ -37,7 +37,21 @@ class MockImageSource:
         :param image : 2-d numpy array to return on 'read'
         :param pixel_size : size of a single pixel, must have a pint unit of type length
         """
-        return MockImageSource(lambda i: image, image.shape, pixel_size.to(u.um))
+        return MockImageSource(lambda: image, image.shape, pixel_size.to(u.um))
+
+    def coordinates(self, d):
+        """Coordinate values along the d-th axis.
+        The coordinates are computed such that position 0.0 corresponds to the pixel at the center,
+        or to the pixel directly after that if the number of pixels is even.
+        """
+        coords = np.array(range(self.data_shape[d]), ndmin=len(self.data_shape))
+        coords = np.moveaxis(coords, -1, d)
+        return (coords - np.floor(0.5 * self.data_shape[d])) * self.pixel_size
+
+    def extent(self):
+        low = -np.floor(0.5 * np.array(self.data_shape))
+        high = np.ceil(0.5 * np.array(self.data_shape))
+        return np.array((low[1], high[1], low[0], high[0])) * self.pixel_size
 
 
 class MockCamera(CropProcessor):