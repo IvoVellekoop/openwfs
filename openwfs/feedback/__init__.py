8e8f812464678e05b5ca8c1fc72ba656905ced38
Jeroen Doornbos <j.h.doornbos@student.utwente.nl>
2023-11-06 16:00:58 +0100

    Rebuild SSA for restructured Controller & added PhaseProcessor

--- openwfs/feedback/__init__.py
+++ openwfs/feedback/__init__.py
@@ -1,2 +1,2 @@
 from . import processors
-from .processors import Processor, SingleRoi, CropProcessor, SingleRoiSquare, SelectRoiSquare, SelectRoiCircle
+from .processors import Processor, SingleRoi, CropProcessor, SingleRoiSquare, SelectRoiSquare, SelectRoiCircle, PhaseProcessor
