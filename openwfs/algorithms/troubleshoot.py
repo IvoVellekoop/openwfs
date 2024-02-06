Fix normalization bug and add unit tests analyze_phase_calibration

openwfs/algorithms/troubleshoot.py
@@ -134,24 +134,35 @@ def measure_setup_stability(frame_source, wait_time_s, num_of_frames):
     return pixel_shifts, correlations
 
 
-def analyze_phase_calibration(wfs_result: WFSResult):
-    """
-    Analyze phase calibration
+def analyze_phase_calibration(wfs_result: WFSResult) -> float:
+    r"""
+    Analyze phase calibration.
+    Estimate the fidelity reduction factor due to wrong phase calibration
+    :math:`\langle|\gamma_\phi|^2\rangle = |c_1|^2 / \sum_{k\neq0} |c_k|^2`,
+    where :math:`c_k` are components of the Fourier transform of phase stepping measurements. For perfectly calibrated
+    SLMs, :math:`\langle|\gamma_\phi|^2\rangle` should converge to 1.
 
     Args:
+        wfs_result: The WFSResult object containing the Fourier transform of the phase stepping measurements.
+            Minimum number of phase steps=5, but a significantly higher number of phase steps is recommended for more
+            accurate estimation.
 
     Returns:
-        Fidelity reduction estimate due to wrong phase calibration
+        Estimate of fidelity reduction due to wrong phase calibration.
     """
     F = wfs_result.t_f  # Fourier transform of phase stepping measurements
+
+    # Prepare indexing
     axis_k = wfs_result.axis  # Axis of phase stepping
-    k_nyquist = int(np.floor((F.shape[axis_k] - 1) / 2))  # Nyquist frequency
-    F1 = np.expand_dims(np.take(F, 1, axis=axis_k), axis=axis_k)
-    Fk_high = np.take(F, range(2, k_nyquist), axis=axis_k)
-    axis_not_k = list(range(F.ndim))
-    axis_not_k.pop(axis_k)
-    inner_sum = np.sum(Fk_high * F1.conj(), axis=tuple(axis_not_k), keepdims=True)
-    return np.sum((np.abs(F1)**2))**2 / np.sum(np.abs(inner_sum)**2, axis=axis_k)
+    axis_not_k = list(range(F.ndim))  # List of all axis indices
+    axis_not_k.pop(axis_k)  # Remove axis of phase stepping
+    k_nyquist = int(np.floor((F.shape[axis_k] - 1) / 2))  # Nyquist frequency index
+
+    # Compute components
+    F1 = np.expand_dims(np.take(F, 1, axis=axis_k), axis=axis_k)  # Base frequency
+    Fk = np.take(F, range(1, k_nyquist), axis=axis_k)  # All frequencies from base to Nyquist
+    inner_sum = np.sum(Fk * F1.conj(), axis=tuple(axis_not_k), keepdims=True)  # Sum over all axes except phase stepping
+    return np.sum((np.abs(F1)**2))**2 / np.sum(np.abs(inner_sum)**2, axis=axis_k)  # Compute |c1|²/∑|ck|²
 
 
 class WFSTroubleshootResult:

tests/test_algorithms_troubleshoot.py
@@ -4,15 +4,10 @@ import astropy.units as u
 import pytest
 
 from ..openwfs.processors import SingleRoi
-from ..openwfs.simulation import Microscope, MockSource, MockSLM
-from ..openwfs.algorithms import FourierDualReference
-from ..openwfs.algorithms.troubleshoot import cnr, signal_std, find_pixel_shift, field_correlation, frame_correlation
-
-
-from ..openwfs.algorithms import StepwiseSequential, FourierDualReference, WFSController
-from ..openwfs.processors import SingleRoi
-from ..openwfs.simulation import SimulatedWFS, MockSource, MockSLM, Microscope, ADCProcessor
-from ..openwfs.algorithms.troubleshoot import analyze_phase_calibration
+from ..openwfs.simulation import SimulatedWFS, MockSource, MockSLM, Microscope
+from ..openwfs.algorithms import StepwiseSequential
+from ..openwfs.algorithms.troubleshoot \
+    import cnr, signal_std, find_pixel_shift, field_correlation, frame_correlation, analyze_phase_calibration
 
 
 def test_signal_std():
@@ -68,12 +63,6 @@ def test_find_pixel_shift():
 def test_field_correlation():
     """
     Test the field correlation, i.e. g_1 normalized first order correlation function.
-    Test the following:
-        self-correlation == 1
-        invariant under scalar multiplication
-        correlation with orthogonal array == 0
-        correlation with arguments swapped
-        correlation with self + orthogonal array
     """
     a = np.zeros(shape=(2, 3))
     a[1, 2] = 2.0
@@ -87,11 +76,11 @@ def test_field_correlation():
     c[1, 0] = 1 + 1j
     c[0, 1] = 2 - 3j
 
-    assert field_correlation(a, a) == 1.0
-    assert field_correlation(2*a, a) == 1.0
-    assert field_correlation(a, b) == 0.0
-    assert np.abs(field_correlation(b, c) - np.conj(field_correlation(c, b))) < 1e-10
-    assert np.abs(field_correlation(a+b, b) - np.sqrt(0.5)) < 1e-10
+    assert field_correlation(a, a) == 1.0                               # Self-correlation
+    assert field_correlation(2*a, a) == 1.0                             # Invariant under scalar-multiplication
+    assert field_correlation(a, b) == 0.0                               # Orthogonal arrays
+    assert np.abs(field_correlation(a+b, b) - np.sqrt(0.5)) < 1e-10     # Self+orthogonal array
+    assert np.abs(field_correlation(b, c) - np.conj(field_correlation(c, b))) < 1e-10  # Arguments swapped
 
 
 def test_frame_correlation():
@@ -108,24 +97,86 @@ def test_frame_correlation():
     assert np.abs(frame_correlation(a, b)) < 2e-3
 
 
-@pytest.mark.parametrize("n_y, n_x", [(5, 5), (7, 11), (6, 4), (30, 20)])
-def test_fidelity_phase_calibration_ssa(n_y, n_x):
+def phase_response_test_function(phi, b, c, gamma):
+    """A synthetic phase response function: 2π*(b + c*(phi/2π)^gamma)"""
+    return np.clip(2*np.pi * (b + c*(phi/(2*np.pi))**gamma), 0, None)
+
+
+def inverse_phase_response_test_function(f, b, c, gamma):
+    """Inverse of the synthetic phase response function: 2π*(b + c*(phi/2π)^gamma)"""
+    return 2*np.pi * ((f/(2*np.pi) - b) / c)**(1/gamma)
+
+
+def lookup_table_test_function(f, b, c, gamma):
     """
-    Test the enhancement performance of the SSA algorithm.
-    Note, for low N, the improvement estimate is not accurate,
-    and the test may sometimes fail due to statistical fluctuations.
+    Compute the lookup indices (i.e. a lookup table)
+    for countering the synthetic phase response test function: 2π*(b + c*(phi/2π)^gamma).
     """
+    phase = inverse_phase_response_test_function(f, b, c, gamma)
+    return (np.mod(phase, 2 * np.pi) * 256 / (2 * np.pi) + 0.5).astype(np.uint8)
+
+
+@pytest.mark.parametrize("n_y, n_x, phase_steps, b, c, gamma",
+                         [(15, 13, 8, -0.05, 1.5, 0.8), (4, 4, 10, -0.05, 1.5, 0.8)])
+def test_fidelity_phase_calibration_ssa_noise_free(n_y, n_x, phase_steps, b, c, gamma):
+    """
+    Test computing phase calibration fidelity factor, with the SSA algorithm. Noise-free scenarios.
+    """
+    # Perfect SLM, noise-free
     aberrations = np.random.uniform(0.0, 2 * np.pi, (n_y, n_x))
     sim = SimulatedWFS(aberrations)
-    alg = StepwiseSequential(feedback=sim, slm=sim.slm, n_x=n_x, n_y=n_y, phase_steps=12)
+    alg = StepwiseSequential(feedback=sim, slm=sim.slm, n_x=n_x, n_y=n_y, phase_steps=phase_steps)
     result = alg.execute()
-    fidelity_phase_calibration = analyze_phase_calibration(result)
-    print(fidelity_phase_calibration)
-    assert fidelity_phase_calibration <= 1
+    fidelity_phase_cal_perfect = analyze_phase_calibration(result)
+    assert np.abs(fidelity_phase_cal_perfect - 1) < 1e-4
+
+    # SLM with incorrect phase response, noise-free
+    linear_phase = np.arange(0, 2*np.pi, 2*np.pi/256)
+    sim.slm.phase_response = phase_response_test_function(linear_phase, b, c, gamma)
+    result = alg.execute()
+    fidelity_wrong_phase_response = analyze_phase_calibration(result)
+    assert fidelity_wrong_phase_response < 0.9
+
+    # SLM calibrated with phase response corrected by LUT, noise-free
+    sim.slm.lookup_table = lookup_table_test_function(linear_phase, b, c, gamma)
+    result = alg.execute()
+    fidelity_phase_cal_lut = analyze_phase_calibration(result)
+    assert np.abs(fidelity_phase_cal_lut - 1) < 1e-3
 
 
+@pytest.mark.parametrize("n_y, n_x, phase_steps, gaussian_noise_std", [(9, 9, 20, 0*0.2), (5, 5, 12, 2.0)])
+def test_fidelity_phase_calibration_ssa_with_noise(n_y, n_x, phase_steps, gaussian_noise_std):
+    """
+    Test estimation of phase calibration fidelity factor, with the SSA algorithm. With noise.
+    """
+    # === Define mock hardware, perfect SLM ===
+    # Aberration and image source
+    numerical_aperture = 1.0
+    aberration_phase = np.random.uniform(0.0, 2 * np.pi, (n_y, n_x))
+    aberration = MockSource(aberration_phase, extent=2 * numerical_aperture)
+    img = np.zeros((64, 64), dtype=np.int16)
+    img[32, 32] = 250
+    src = MockSource(img, 500 * u.nm)
 
-@pytest.mark.skip(reason="This test does not test anything yet and gives a popop graph.")
+    # SLM, simulation, camera, ROI detector
+    slm = MockSLM(shape=(80, 80))
+    sim = Microscope(source=src, slm=slm.pixels(), magnification=1, numerical_aperture=numerical_aperture,
+                     aberrations=aberration, wavelength=800 * u.nm)
+    cam = sim.get_camera(analog_max=1e4, gaussian_noise_std=gaussian_noise_std)
+    roi_detector = SingleRoi(cam, radius=0)  # Only measure that specific point
+
+    linear_phase = np.arange(0, 2*np.pi, 2*np.pi/256)
+    slm.phase_response = phase_response_test_function(linear_phase, b=0.05, c=0.7, gamma=1.4)
+
+    # Define and run WFS algorithm
+    alg = StepwiseSequential(feedback=roi_detector, slm=slm, n_x=n_x, n_y=n_y, phase_steps=phase_steps)
+    result_good = alg.execute()
+    phase_pattern_good = np.angle(result_good.t)
+    fidelity_phase_cal_noise = analyze_phase_calibration(result_good)
+    assert np.abs(fidelity_phase_cal_noise - 1) < 0.005
+
+
+@pytest.mark.skip(reason="This test does not test anything yet and gives a popup graph.")
 def test_wfs_troubleshooter():
     # Define mock hardware
     numerical_aperture = 1.0

tests/test_simulation.py
@@ -209,7 +209,7 @@ def test_mock_slm_lut_and_phase_response():
     slm3.set_phases(linear_phase)
     assert np.all(np.abs(slm3.phases - inverse_phase_response_test_function(linear_phase, b, c, gamma)) < (1.1*np.pi/256))
 
-    # === Test custom lookup table that counters custom synthetic phase response
+    # === Test custom lookup table that counters custom synthetic phase response ===
     linear_phase_highres = np.arange(0, 2*np.pi*255.49/256, 0.25*2*np.pi/256)
     slm4 = MockSLM(shape=(3, linear_phase_highres.shape[0]))
     slm4.phase_response = phase_response