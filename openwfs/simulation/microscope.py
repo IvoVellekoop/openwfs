refactored project/place and fixed off-by-one error

openwfs/simulation/microscope.py
@@ -8,6 +8,7 @@ from ..core import Processor, Detector
 from ..utilities import project, place, Transform, set_pixel_size, get_pixel_size, patterns
 from ..processors import TransformProcessor
 import warnings
+from ..plot_utilities import imshow  # noqa - for debugging
 
 
 class Microscope(Processor):
@@ -173,7 +174,6 @@ class Microscope(Processor):
 
         # condition 2. Minimum number of pixels in x and y should be data_shape
         pupil_shape = self.data_shape
-        pupil_pixel_size = pupil_extent / pupil_shape
 
         # Compute the field in the pupil plane
         # The aberrations and the SLM phase pattern are both mapped to the pupil plane coordinates
@@ -185,13 +185,16 @@ class Microscope(Processor):
 
         if aberrations is not None and slm is not None:
             pupil_field = pupil_field * np.exp(1.0j * (
-                    project(pupil_shape, pupil_pixel_size, aberrations, self.aberration_transform) +
-                    project(pupil_shape, pupil_pixel_size, slm, self.slm_transform)))
+                    project(aberrations, out_extent=pupil_extent, out_shape=pupil_shape,
+                            transform=self.aberration_transform) +
+                    project(slm, out_extent=pupil_extent, out_shape=pupil_shape, transform=self.slm_transform)))
         elif slm is not None:
-            pupil_field = pupil_field * np.exp(1.0j * project(pupil_shape, pupil_pixel_size, slm, self.slm_transform))
+            pupil_field = pupil_field * np.exp(
+                1.0j * project(slm, out_shape=pupil_shape, out_extent=pupil_extent, transform=self.slm_transform))
         elif aberrations is not None:
             pupil_field = pupil_field * np.exp(
-                1.0j * project(pupil_shape, pupil_pixel_size, aberrations, self.aberration_transform))
+                1.0j * project(aberrations, out_extent=pupil_extent, out_shape=pupil_shape,
+                               transform=self.aberration_transform))
 
         # Compute the point spread function
         # This is done by Fourier transforming the pupil field and taking the absolute value squared

openwfs/simulation/mockdevices.py
@@ -1,13 +1,12 @@
 import numpy as np
 import astropy.units as u
 from astropy.units import Quantity
-from scipy.ndimage import zoom
 from numpy.typing import ArrayLike
 import time
 from typing import Sequence, Optional
 from ..processors import CropProcessor
 from ..core import Detector, Processor, PhaseSLM, Actuator
-from ..utilities import ExtentType, get_pixel_size
+from ..utilities import ExtentType, get_pixel_size, project, set_extent, get_extent
 
 
 class Generator(Detector):
@@ -212,7 +211,8 @@ class ADCProcessor(Processor):
             dtype = data.dtype
             rng = np.random.default_rng()
             gaussian_noise = self._gaussian_noise_std * rng.standard_normal(size=data.shape)
-            data = (self._signal_multiplier*data.astype('float64') + gaussian_noise).clip(0, self.digital_max).astype(dtype)
+            data = (self._signal_multiplier * data.astype('float64') + gaussian_noise).clip(0, self.digital_max).astype(
+                dtype)
 
         if self.analog_max == 0.0:
             max = np.max(data)
@@ -424,18 +424,27 @@ class MockSLM(PhaseSLM, Actuator):
         super().__init__()
         if len(shape) != 2:
             raise ValueError("Shape of the SLM should be 2-dimensional.")
-
+        self.shape = shape
         self._requested_phases = np.zeros(shape, 'float32')
         self._grey_values = np.zeros(shape, 'uint8')  # Stored grey value: 0 -> 0rad, 255 -> almost 2π
-        self._monitor = MockSource(self._requested_phases, pixel_size=2.0 / np.min(shape) * u.dimensionless_unscaled)  # Actual phase
-        self._phase_response = np.arange(0, 2*np.pi, 2*np.pi/256)  # index = input grey value, value = output phase
+        self._monitor = MockSource(self._requested_phases,
+                                   pixel_size=2.0 / np.min(shape) * u.dimensionless_unscaled)  # Actual phase
+        self._phase_response = np.arange(0, 2 * np.pi,
+                                         2 * np.pi / 256)  # index = input grey value, value = output phase
         self.lookup_table = range(256)  # index = input phase (scaled to -> [0, 255]), value = grey value
 
     def update(self):
         self._start()  # wait for detectors to finish
 
         # Apply lookup table and compute grey values from intended phases
-        lookup_index = (np.mod(self._requested_phases, 2 * np.pi) * 256 / (2 * np.pi) + 0.5).astype(np.uint8)
+        # This uses the same conversion as in the shader:
+        # first compute tx = phi * / (2 * pi) + 0.5 / 256
+        # use the fractional part of tx to index the lookup table, where:
+        # - tx =0 maps to the start of the first element
+        # - tx=1-δ maps to the end of the last element
+        tx = self._requested_phases * (1 / (2 * np.pi)) + (0.5 / 256)
+        tx = tx - np.floor(tx)  # fractional part of tx
+        lookup_index = np.floor(self._lookup_table.shape[0] * tx).astype(np.uint8)  # index into lookup table
         self._grey_values = self._lookup_table[lookup_index]
 
         # Apply phase_response and return actual phases
@@ -470,10 +479,9 @@ class MockSLM(PhaseSLM, Actuator):
 
     def set_phases(self, values: ArrayLike, update=True):
         # no docstring, use documentation from base class
-        values = np.atleast_2d(values)
-        scale = np.array(self._requested_phases.shape) / np.array(values.shape)
-        # TODO: replace by cv2, with area interpolation
-        zoom(values, scale, output=self._requested_phases, order=0)
+
+        project(np.atleast_2d(values).astype('float32'), out=self._requested_phases, source_extent=(2.0, 2.0),
+                out_extent=(2.0, 2.0))
         if update:
             self.update()
 

tests/test_utilities.py
@@ -20,18 +20,11 @@ def test_to_matrix():
     result_matrix = transform.to_matrix((0.5, 4) * u.um, (1, 2) * u.um)
     assert np.allclose(result_matrix, expected_matrix)
 
-    # Check if we get the correct matrix in CV2 format without center correction
-    expected_matrix = ((8, 1.5, 1), (4, 0.5, 1))
-    result_matrix = transform.cv2_matrix(source_shape=(0, 0),
-                                         source_pixel_size=(0.5, 4) * u.um, destination_shape=(0, 0),
-                                         destination_pixel_size=(1, 2) * u.um)
-    assert np.allclose(result_matrix, expected_matrix)
-
     # Test center correction. The center of the source image should be mapped to the center of the destination image
     src = (17, 18)
     dst = (13, 14)
-    src_center = np.array((0.5 * src[1], 0.5 * src[0], 1.0))
-    dst_center = np.array((0.5 * dst[1], 0.5 * dst[0]))
+    src_center = np.array((0.5 * (src[1] - 1), 0.5 * (src[0] - 1), 1.0))
+    dst_center = np.array((0.5 * (dst[1] - 1), 0.5 * (dst[0] - 1)))
     transform = Transform()
     result_matrix = transform.cv2_matrix(source_shape=src,
                                          source_pixel_size=(1, 1), destination_shape=dst,
@@ -98,13 +91,15 @@ def test_transform():
 
     # shift both origins by same distance
     t0 = Transform(source_origin=-ps1 * (1.7, 2.2), destination_origin=-ps1 * (1.7, 2.2))
-    dst0 = project(src.shape, ps1, src, t0)
+    dst0 = project(src, source_extent=ps1 * np.array(src.shape), transform=t0, out_extent=ps1 * np.array(src.shape),
+                   out_shape=src.shape)
     assert np.allclose(dst0, src)
 
     # shift source by (1,2) pixel
     t1 = Transform(source_origin=-ps1 * (1, 2))
     dst1a = place(src.shape, ps1, src, offset=ps1 * (1, 2))
-    dst1b = project(src.shape, ps1, src, t1)
+    dst1b = project(src, source_extent=ps1 * np.array(src.shape), transform=t1, out_extent=ps1 * np.array(src.shape),
+                    out_shape=src.shape)
     assert np.allclose(dst1a, dst1b)
 
 