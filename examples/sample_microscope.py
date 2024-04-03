fixed units and plotting

examples/sample_microscope.py
@@ -9,9 +9,9 @@ import astropy.units as u
 import numpy as np
 
 import set_path  # noqa - needed for setting the module search path to find openwfs
-from openwfs.plot_utilities import grab_and_show
+from openwfs.plot_utilities import grab_and_show, imshow
 from openwfs.simulation import Microscope, StaticSource
-from openwfs.utilities import Transform
+from openwfs.utilities import set_pixel_size
 
 # Parameters that can be altered
 
@@ -40,25 +40,28 @@ p_limit = 100
 # Number of iterations. Influences how quick the 'animation' is complete.
 
 # Code
-img = np.maximum(np.random.randint(-10000, 100, (img_size_y, img_size_x), dtype=np.int16), 0)
-src = StaticSource(img, 50 * u.nm)
+img = set_pixel_size(
+    np.maximum(np.random.randint(-10000, 100, (img_size_y, img_size_x), dtype=np.int16), 0),
+    60 * u.nm)
+src = StaticSource(img)
 mic = Microscope(src, magnification=magnification, numerical_aperture=numerical_aperture, wavelength=wavelength)
 
 # simulate shot noise in an 8-bit camera with auto-exposure:
-cam = mic.get_camera(shot_noise=True, digital_max=255, transform=Transform.zoom(2.0))
+cam = mic.get_camera(shot_noise=True, digital_max=255, data_shape=camera_resolution, pixel_size=pixel_size)
 devices = {'camera': cam, 'stage': mic.xy_stage}
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     plt.subplot(1, 2, 1)
-    plt.imshow(img)
+    imshow(img)
     plt.title('Original image')
-    ax = plt.subplot(1, 2, 2)
+    plt.subplot(1, 2, 2)
     plt.title('Scanned image')
+    ax = None
     for p in range(p_limit):
-        # mic.xy_stage.x = p * 1 * u.um
+        mic.xy_stage.x = p * 1 * u.um
         mic.numerical_aperture = 1.0 * (p + 1) / p_limit  # NA increases to 1.0
-        grab_and_show(cam)
+        ax = grab_and_show(cam, ax)
         plt.title(f"NA: {mic.numerical_aperture}, δ: {mic.abbe_limit.to_value(u.um):2.2} μm")
         plt.pause(0.2)

openwfs/processors/processors.py
@@ -1,11 +1,11 @@
-from typing import Sequence, Optional, Union
+from typing import Sequence, Optional
 
 import cv2
 import numpy as np
 from astropy.units import Quantity
 
 from ..core import Processor, Detector
-from ..utilities import project
+from ..utilities import project, Transform
 from ..utilities.patterns import disk, gaussian
 
 
@@ -338,39 +338,49 @@ def select_roi(source: Detector, mask_type: str):
 class TransformProcessor(Processor):
     """
     Performs a 2-D transform of the input data (including shifting, padding, cropping, resampling).
+
+    By default, the output shape and pixel_size are the same as the input shape.
+    If desired, explicit values can be provided for the output shape and pixel_size. The unit of the pixel_size
+    should match the unit of the input data after applying the transform.
     """
 
-    def __init__(self, source, transform, data_shape: Optional[Sequence[int]] = None,
-                 pixel_size: Union[Optional[Quantity], bool] = False, multi_threaded: bool = True):
+    def __init__(self, source: Detector,
+                 transform: Transform = None,
+                 data_shape: Optional[Sequence[int]] = None,
+                 pixel_size: Optional[Quantity] = None,
+                 multi_threaded: bool = True):
         """
 
         Args:
-            transform: Transform object that describes the transformation from the source
-            data_shape: defaults to source.data_shape
-            pixel_size: defaults to source.pixel_size.
-                Specify any pixel size (including `None`) to override the pixel size of the source.
-        """
+            transform: Transform object that describes the transformation from the source to the target image
+            data_shape: Shape of the output. If omitted, the shape of the input data is used.
+            multi_threaded: Whether to perform processing in a worker thread.
+            """
         if (data_shape is not None and len(data_shape) != 2) or len(source.data_shape) != 2:
             raise ValueError("TransformProcessor only supports 2-D data")
+        if transform is None:
+            transform = Transform()
 
+        # check if input and output pixel sizes are compatible
+        dst_unit = transform.destination_unit(source.pixel_size.unit)
+        if pixel_size is not None and not pixel_size.unit.is_equivalent(dst_unit):
+            raise ValueError("Pixel size unit does not match the unit of the transformed data")
+        if pixel_size is None and not source.pixel_size.unit.is_equivalent(dst_unit):
+            raise ValueError("The transform changes the unit of the coordinates."
+                             " An output pixel_size must be provided.")
+
+        self.transform = transform
+        super().__init__(source, multi_threaded=multi_threaded)
         self._pixel_size = pixel_size
         self._data_shape = data_shape
-        super().__init__(source, multi_threaded=multi_threaded)
-        self.transform = transform
 
     @property
     def data_shape(self):
-        if self._data_shape is not None:
-            return self._data_shape
-        else:
-            return super().data_shape
+        return self._data_shape if self._data_shape is not None else super().data_shape
 
     @property
     def pixel_size(self) -> Optional[Quantity]:
-        if self._pixel_size is not False:
-            return self._pixel_size
-        else:
-            return super().pixel_size
+        return self._pixel_size if self._pixel_size is not None else super().pixel_size
 
     def _fetch(self, source: np.ndarray) -> np.ndarray:  # noqa
         """

openwfs/utilities/utilities.py
@@ -52,10 +52,15 @@ def unitless(data: ArrayLike) -> np.ndarray:
 
 @dataclass
 class Transform:
-    """Represents a transformation from one coordinate system to the other.
+    """Represents a transformation from one 2-d coordinate system to another.
 
     Transform objects are used to specify any combination of shift, (anisotropic) scaling, rotation and shear.
-    Elements of the transformation are specified with an `astropy.unit` attached.
+    The transform matrix, as well as the source and destination origins, can be specified with astropy units attached.
+
+    Note that a Transform object does not contain any information about the extent and pixel size
+    of the source or destination image, it only specifies the coordinate transformation itself.
+    When a Transform object is used to transform image data (see :func:`project` and :func:`place`),
+    the pixel_size information should be specified separately.
     """
 
     """
@@ -92,13 +97,33 @@ class Transform:
         self.source_origin = Quantity(source_origin) if source_origin is not None else None
         self.destination_origin = Quantity(destination_origin) if destination_origin is not None else None
 
-    def cv2_matrix(self, source_shape: Sequence[int],
+        if source_origin is not None:
+            self.destination_unit(self.source_origin.unit)  # check if the units are consistent
+
+    def destination_unit(self, src_unit: u.Unit) -> u.Unit:
+        """Computes the unit of the output of the transformation, given the unit of the input.
+
+        Raises:
+            ValueError: If src_unit does not match the unit of the source_origin (if specified) or
+                if dst_unit does not match the unit of the destination_origin (if specified).
+        """
+        if self.source_origin is not None and not self.source_origin.unit.is_equivalent(src_unit):
+            raise ValueError("src_unit must match the units of source_origin.")
+
+        dst_unit = (self.transform[0, 0] * src_unit).unit
+        if self.destination_origin is not None and not self.destination_origin.unit.is_equivalent(dst_unit):
+            raise ValueError("dst_unit must match the units of destination_origin.")
+
+        return dst_unit
+
+    def cv2_matrix(self,
+                   source_shape: Sequence[int],
                    source_pixel_size: CoordinateType,
                    destination_shape: Sequence[int],
                    destination_pixel_size: CoordinateType) -> np.ndarray:
         """Returns the transformation matrix in the format used by cv2.warpAffine."""
 
-        # correct the origin. OpenCV uses the _center_ of teh top-left corner as the origin
+        # correct the origin. OpenCV uses the _center_ of the top-left corner as the origin
         # and openwfs uses the center of the image as the origin, so we need to shift the origin
         # by half the image size minus half a pixel.
         if min(source_shape) < 1 or min(destination_shape) < 1:
@@ -229,12 +254,13 @@ def place(out_shape: tuple[int, ...], out_pixel_size: Quantity, source: np.ndarr
     return project(source, out_extent=out_extent, out_shape=out_shape, transform=transform, out=out)
 
 
-def project(source: np.ndarray, *,
-            transform: Optional[Transform] = None,
-            out: Optional[np.ndarray] = None,
-            source_extent: Optional[ExtentType] = None,
-            out_extent: Optional[ExtentType] = None,
-            out_shape: Optional[tuple[int, ...]] = None) -> np.ndarray:
+def project(
+        source: np.ndarray, *,
+        source_extent: Optional[ExtentType] = None,
+        transform: Optional[Transform] = None,
+        out: Optional[np.ndarray] = None,
+        out_extent: Optional[ExtentType] = None,
+        out_shape: Optional[tuple[int, ...]] = None) -> np.ndarray:
     """Projects the input image onto an array with specified shape and resolution.
 
     The input image is scaled so that the pixel sizes match those of the output,