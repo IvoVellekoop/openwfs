f051de98f4aabfa1d1968502ac35cd58ca9fcdc9
Daniel Cox <danielcox16@hotmail.com>
2024-07-19 19:01:09 +0200

    Add k_step factor for FDR

--- openwfs/algorithms/basic_fourier.py
+++ openwfs/algorithms/basic_fourier.py
@@ -8,7 +8,7 @@ from .fourier import FourierBase
 from ..core import Detector, PhaseSLM
 
 
-def build_square_k_space(k_min, k_max):
+def build_square_k_space(k_min, k_max, k_step=1.0):
     """
     Constructs the k-space by creating a set of (k_x, k_y) coordinates.
     Fills the k_left and k_right matrices with the same k-space. (k_x, k_y) denote the k-space coordinates of the whole
@@ -25,7 +25,7 @@ def build_square_k_space(k_min, k_max):
     # Combine kx and ky coordinates into pairs
     k_x = np.repeat(np.array(kx_angles)[np.newaxis, :], len(ky_angles), axis=0).flatten()
     k_y = np.repeat(np.array(ky_angles)[:, np.newaxis], len(kx_angles), axis=1).flatten()
-    k_space = np.vstack((k_x, k_y))
+    k_space = np.vstack((k_x, k_y)) * k_step
     return k_space
 
 
@@ -108,7 +108,7 @@ class FourierDualReferenceCircle(FourierBase):
     "Wavefront shaping for forward scattering," Opt. Express 30, 37436-37445 (2022)
     """
     def __init__(self, feedback: Detector, slm: PhaseSLM, slm_shape=(500, 500), phase_steps=4, k_radius: float = 3.2,
-                 analyzer: Optional[callable] = analyze_phase_stepping):
+                 k_step: float = 1.0, analyzer: Optional[callable] = analyze_phase_stepping):
         """
         Args:
             feedback (Detector): Source of feedback
@@ -116,6 +116,7 @@ class FourierDualReferenceCircle(FourierBase):
             slm_shape (tuple of two ints): The shape that the SLM patterns & transmission matrices are calculated for,
                 does not necessarily have to be the actual pixel dimensions as the SLM.
             k_radius (float): Limit grid points to lie within a circle of this radius.
+            k_step (float): Make steps in k-space of this value. 1 corresponds to diffraction limited tilt.
             phase_steps (int): The number of phase steps.
         """
         # TODO: Could be rewritten more compactly if we ditch the settable properties:
@@ -126,6 +127,7 @@ class FourierDualReferenceCircle(FourierBase):
                          k_right=np.array((0, 0)), phase_steps=phase_steps, analyzer=analyzer)
 
         self._k_radius = k_radius
+        self.k_step = k_step
         self._build_k_space()
 
     def _build_k_space(self):
@@ -138,8 +140,9 @@ class FourierDualReferenceCircle(FourierBase):
             None: The function updates the instance attributes k_left and k_right.
         """
         k_radius = self.k_radius
+        k_step = self.k_step
         k_max = int(np.floor(k_radius))
-        k_space_square = build_square_k_space(-k_max, k_max)
+        k_space_square = build_square_k_space(-k_max, k_max, k_step=k_step)
 
         # Filter out k-space coordinates that are outside the circle of radius k_radius
         k_mask = (np.linalg.norm(k_space_square, axis=0) <= k_radius)
