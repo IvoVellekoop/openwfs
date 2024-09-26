cc706a6dd69dff95356d69dcf2639867c4c64b45
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2024-09-26 15:51:57 +0200

    fixed imports for optional packages

--- openwfs/devices/__init__.py
+++ openwfs/devices/__init__.py
@@ -1,7 +1,14 @@
-from . import galvo_scanner
-from . import nidaq_gain
+try:
+    from .camera import Camera
+except ImportError:
+    pass  # ok, we don't have harvesters installed
+
+try:
+    from . import galvo_scanner
+    from .galvo_scanner import ScanningMicroscope, Axis
+    from .nidaq_gain import Gain
+except ImportError:
+    pass  # ok, we don't have nidaqmx installed
+
 from . import slm
-from .camera import Camera
-from .galvo_scanner import ScanningMicroscope, Axis
-from .nidaq_gain import Gain
 from .slm import SLM
