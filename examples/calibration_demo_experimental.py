fe1dea7d99cf980bc48ce99f0801f17d6822197a
Daniel Cox <danielcox16@hotmail.com>
2024-11-14 15:08:37 +0100

    Update calibration demo

--- examples/calibration_demo_experimental.py
+++ examples/calibration_demo_experimental.py
@@ -10,19 +10,33 @@ import numpy as np
 
 from openwfs.calibration import FringeAnalysisSLMCalibrator
 from openwfs.devices import Camera, SLM
+from openwfs.simulation import Camera as MockCam
+
+
+repetitions = 5     # How often to run the experiment (for e.g. error bars)
 
 # This script shows how a wavefront shaping experiment can be performed from Python
-cam = Camera(R"C:\Program Files\Basler\pylon 7\Runtime\x64\ProducerU3V.cti")
-cam.exposure_time = 16.666 * u.ms
+cam = Camera(R"C:\Program Files\Basler\pylon 6\Runtime\x64\ProducerU3V.cti")
+cam.exposure_time = 90 * u.ms
+
 
 # constructs the actual slm for wavefront shaping, and a monitor window to display the current phase pattern
 slm = SLM(2)
 monitor = slm.clone(monitor_id=0, pos=(0, 0), shape=(slm.shape[0] // 4, slm.shape[1] // 4))
 
-calibrator = FringeAnalysisSLMCalibrator(camera=cam, slm=slm)
-field, gray_values, frames = calibrator.execute()
+s = cam.data_shape
+modulated_slices = (slice(None), slice(s[0] // 6, s[0] // 3))
+reference_slices = (slice(None), slice(-s[0] // 3, -s[0] // 6))
+calibrator = FringeAnalysisSLMCalibrator(
+    camera=cam, slm=slm, modulated_slices=modulated_slices, reference_slices=reference_slices
+)
 
-plt.figure()
-phase = np.unwrap(np.angle(field))
-plt.plot(phase)
-plt.show()
+for r in range(repetitions):
+    field, gray_values, frames = calibrator.execute()
+
+    np.savez(
+        f'C:/LocalData/twymann-green-slm-calibration-r{r}.npz',
+        frames=frames,
+        field=field,
+        gray_values=gray_values,
+    )
