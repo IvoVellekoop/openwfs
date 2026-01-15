abfe162bb4fde027705b77b80fa3dc0c89ec90b2
DylanMMarques <rmapdm1@ucl.ac.uk>
2026-01-15 13:55:01 +0100

    add function to return a microscope imaging a point source

---
+++ openwfs/utilities/tests.py
@@ -0,0 +1,44 @@
+import numpy as np
+import astropy.units as u
+from ..simulation import SLM, Microscope, StaticSource
+
+def get_microscope_setup():
+    """
+        Convenience function returning a basic microscope setup for testing purposes. The microscope has a static source representing a point source, and an SLM as incident field. The microscope parameters are:
+        - Specimen resolution: (512, 512) pixels
+        - Specimen pixel size: 100 nm
+        - Numerical aperture: 0.85
+        - Wavelength: 532.8 nm
+        - Incident field: SLM with shape (512, 512).
+
+        Returns:
+            mic: Microscope
+                The microscope object.
+            slm: SLM
+                The SLM object used as incident field.
+            src: StaticSource
+                The static source object representing the specimen.
+    """
+    specimen_resolution = (512, 512)
+    specimen_pixel_size = 100.0 * u.nm
+    numerical_aperture = 0.85
+    wavelength = 532.8 * u.nm
+    img_plane = np.zeros(specimen_resolution)
+    img_plane[100:102, 400:402] = 1.0
+
+    src = StaticSource(
+        data=img_plane,
+        pixel_size=specimen_pixel_size,
+    )
+
+    slm = SLM(shape=(512, 512))
+
+    mic = Microscope(
+        src,
+        magnification=1.0,
+        numerical_aperture=numerical_aperture,
+        wavelength=wavelength,
+        incident_field=slm.field,
+    )
+    return mic, slm, src
+
