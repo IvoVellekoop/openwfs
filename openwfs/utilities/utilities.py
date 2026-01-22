52cd3d54c26852d505479c5699b25c29574fed69
DylanMMarques <rmapdm1@ucl.ac.uk>
2026-01-22 14:14:04 +0100

    check in set_pixel_size that old and new pixel_size are different

--- openwfs/utilities/utilities.py
+++ openwfs/utilities/utilities.py
@@ -396,8 +396,10 @@ def set_pixel_size(data: ArrayLike, pixel_size: Optional[Quantity]) -> np.ndarra
     if pixel_size is not None and pixel_size.size == 1:
         pixel_size = pixel_size * np.ones(data.ndim)
 
-    if get_pixel_size(data) is not None:
-        raise NotImplementedError("Overwritting the value of pixel size is currently not possible.")
+    old_pixel_size = get_pixel_size(data)
+    if old_pixel_size is not None:
+        if not np.allclose(old_pixel_size, pixel_size):
+            raise NotImplementedError("Overwritting the value of pixel size is currently not possible.")
 
     data.dtype = np.dtype(data.dtype, metadata={"pixel_size": pixel_size})
     return data
