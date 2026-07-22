4e71f2b46e19c08bf7a2208dae3f3f2eefd50232
ArnonBAB <kruimpie4@gmail.com>
2026-07-22 13:54:08 +0200

    fix bugs and tested with physical device

--- openwfs/devices/meadowlark_hdmi.py
+++ openwfs/devices/meadowlark_hdmi.py
@@ -3,6 +3,7 @@ import weakref
 from openwfs.devices import SLM
 import numpy as np
 import tempfile
+import astropy.units as u
 
 class BlinkHDMIHandler:
     """
@@ -19,7 +20,7 @@ class BlinkHDMIHandler:
         Add a file to the Blink software.
         :param file_path: The path to the file to be added.
         """
-        if self.path is not None:
+        if self.path is None:
             self.path = path
             ctypes.cdll.LoadLibrary(self.path)
             self.blink_lib = ctypes.CDLL("Blink_C_wrapper")
@@ -28,7 +29,8 @@ class BlinkHDMIHandler:
                 raise ValueError("A different DLL has already been loaded.")
 
         if not self.sdk_created:
-            self.blink_lib.CreateSDK()
+            self.blink_lib.Create_SDK()
+            self.blink_lib.Get_SLMTemp.restype = ctypes.c_double # Taken from example file
             self.sdk_created = True
 
     def create_sdk(self):
@@ -55,13 +57,13 @@ class BlinkHDMIHandler:
         Destructor for the BlinkHDMIHandler class. This method is called when the object is deleted and ensures that the Blink software is properly closed.
         """
         if self.sdk_created:
-            self.blink_lib.CloseSDK()
+            self.blink_lib.Delete_SDK()
             self.sdk_created = False
 
 global_blinkhdmi_handler = None
 
 
-class SLMBLinkHDMI(SLM):
+class SLMBlinkHDMI(SLM):
     
 
     def __init__(self, blink_path, slm_index = 0, monitor_id = None, is_10bit = False):
@@ -71,7 +73,7 @@ class SLMBLinkHDMI(SLM):
         self.is_10bit = is_10bit
         
         buffer = ctypes.create_unicode_buffer(256)
-        self.usb_port = self.handler.blink_lib.GetCOMPort(self.slm_blink_index, buffer)
+        self.usb_port = self.handler.blink_lib.GetComPort(self.slm_blink_index, buffer)
 
         super().__init__(monitor_id = monitor_id)
 
@@ -105,7 +107,10 @@ class SLMBLinkHDMI(SLM):
     def load_linear_lookup_table(self):
         bit_depth = 10 if self.is_10bit else 8
         bit_grey = np.arange(2 ** bit_depth)
-        bit_voltage = bit_depth * 4 # Map the 8/10 bit values to the 10/12 bit range
+        bit_voltage = bit_grey * 4 # Map the 8/10 bit values to the 10/12 bit range
         self.load_lookup_table(bit_grey, bit_voltage)
 
+    @property
+    def temperature(self):
+        return self.handler.blink_lib.Get_SLMTemp(self.slm_blink_index) * u.deg_C
 
