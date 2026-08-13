953db373d95509b882cdf2b7eecd25e19111b659
ArnonBAB <a.b.ab@utwente.nl>
2026-08-13 15:53:12 +0200

    add calibratedSLM
    can be used to match physical properties of the mock slm to an slm in an experimental setup. Allows for simulating filters present in an experimental  setup to simulate the effect of filters on the field, for example in the case of limited NA.

---
+++ openwfs/simulation/calibratedSLM.py
@@ -0,0 +1,72 @@
+from openwfs.devices.slm import SLM
+import numpy as np
+from openwfs.devices.slm.slm import FrameBufferReader
+from openwfs.simulation.slm import PhaseToField
+from openwfs.utilities.patterns import coordinate_range
+from private_openwfs.core import Detector, Processor
+from typing import Callable
+
+class CalibratedSLM(SLM):
+    
+    def __init__(self, filter: Callable[[np.ndarray], np.ndarray], physical_size: tuple, wavelength: float, modulated_field_amplitude: np.ndarray, non_modulated_field_fraction: float = 0.0, *args, **kwargs):           
+        super().__init__(*args, **kwargs)
+
+        if modulated_field_amplitude.shape != self.shape:
+            raise ValueError("modulated_field_amplitude must have the same shape as the SLM shape.")
+        
+        self.filter = filter
+        self.physical_size = physical_size
+        self.wavelength = wavelength
+        self.modulated_field_amplitude = modulated_field_amplitude
+        self.non_modulated_field_fraction = non_modulated_field_fraction
+        kx,ky = self.pupil2kspace_coordinates()
+
+
+    def pupil2kspace_coordinates(self):
+        """
+        Convert pupil coordinates to k-space coordinates.
+        """
+        # Assuming the SLM has a square shape
+        slm_shape = self.shape
+        transform = self.transform
+
+        # convert pupil coordinates to k-space coordiants
+        if self._coordiante_system == "full":
+            slm_pupil_coords = coordinate_range(slm_shape, extent = 2)
+            slm_pupil_coords_y = slm_pupil_coords[0]
+            slm_pupil_coords_x = slm_pupil_coords[1]
+            kx = None
+            ky = None
+        else: 
+            raise ValueError("Only full coordinate system is supported for pupil to k-space conversion.")
+
+            # convert pupil coordinates to k-space coordinates
+        return kx, ky    
+
+
+    @property
+    def field(self) -> Detector:
+        """
+        Return a Detector that computes the field at the SLM plane given the SLM phases.
+        """
+        return FilteredPhaseToField(self.phases, self.modulated_field_amplitude, self.non_modulated_field_fraction, self.filter)
+
+
+class FilteredPhaseToField(PhaseToField):
+    """
+    Take a phase as input, build the field and filter it with the SLM filter.
+    Computes 'Filter[moduled_field_amplitude * exp(1j * phase) + non_modulated_field_fraction]'.
+     """
+
+    def __init__(self, slm_phases: Detector, modulated_field_amplitude: np.ndarray, slm_filter: Callable[ [np.ndarray], np.ndarray ], non_modulated_field_fraction: float = 0.0):
+        self.slm_phases = slm_phases
+        self.modulated_field_amplitude = modulated_field_amplitude
+        self.non_modulated_field_fraction = non_modulated_field_fraction
+        self.slm_filter = slm_filter
+        super().__init__(slm_phases, modulated_field_amplitude, non_modulated_field_fraction, multiprocessing=False)
+
+    def _fetch(self) -> np.ndarray:
+        """
+        Compute the field at the SLM plane given the SLM phases.
+        """
+        return self.slm_filter(PhaseToField.read())
