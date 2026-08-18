4dba130bebfbdbbc3bd92cbc5d42b087d90c9341
ArnonBAB <a.b.ab@utwente.nl>
2026-08-18 13:56:59 +0200

    add docstring + cleanup

--- openwfs/simulation/calibratedSLM.py
+++ openwfs/simulation/calibratedSLM.py
@@ -3,50 +3,51 @@ import numpy as np
 from openwfs.devices.slm.slm import FrameBufferReader
 from openwfs.simulation.slm import PhaseToField
 from openwfs.utilities.patterns import coordinate_range
-from private_openwfs.core import Detector, Processor
+from openwfs.core import Detector, Processor
 from typing import Callable
 
 class CalibratedSLM(SLM):
+    """
+    A callibrated SLM that matches the physics properties of the real SLM.
     
-    def __init__(self, filter: Callable[[np.ndarray], np.ndarray], physical_size: tuple, wavelength: float, modulated_field_amplitude: np.ndarray, non_modulated_field_fraction: float = 0.0, *args, **kwargs):           
-        # check if no coordinate system is given
+    Args:
+        filter: A callable that takes a field and returns the filtered field.
+        physical_size: The physical size of the SLM using astropy units (width, height).
+        wavelength: The wavelength of the light in meters.
+    """
+    def __init__(self, physical_size: tuple, wavelength: float, filter: Callable[[np.ndarray], np.ndarray] = None,*args, **kwargs):           
         super().__init__(*args, **kwargs)
 
-        if modulated_field_amplitude.shape != self.shape:
-            raise ValueError("modulated_field_amplitude must have the same shape as the SLM shape.")
+        if not np.isscalar(self.amplitude) and self.amplitude.shape != self.shape:
+            raise ValueError("amplitude must have the same shape as the SLM shape.")
         
         self.filter = filter
         self.physical_size = physical_size
         self.wavelength = wavelength
-        self.modulated_field_amplitude = modulated_field_amplitude
-        self.non_modulated_field_fraction = non_modulated_field_fraction
-        kx,ky = self.pupil2kspace_coordinates()
 
 
-    def pupil2kspace_coordinates(self):
-        """
-        Convert pupil coordinates to k-space coordinates.
-        """
-        # Assuming the SLM has a square shape
-        slm_shape = self.shape
-        transform = self.transform
+    # def pupil2kspace_coordinates(self):
+    #     """
+    #     Convert pupil coordinates to k-space coordinates.
+    #     """
+    #     # Assuming the SLM has a square shape
+    #     slm_shape = self.shape
+    #     transform = self.transform
 
-        # convert pupil coordinates to k-space coordiants
-        if self._coordiante_system == "full":
-            slm_pupil_coords = coordinate_range(slm_shape, extent = 2)
-            slm_pupil_coords_y = slm_pupil_coords[0]
-            slm_pupil_coords_x = slm_pupil_coords[1]
-            kx = None
-            ky = None
-        else: 
-            raise ValueError("Only full coordinate system is supported for pupil to k-space conversion.")
+    #     # convert pupil coordinates to k-space coordiants
+    #     if self._coordiante_system == "full":
+    #         slm_pupil_coords = coordinate_range(slm_shape, extent = 2)
+    #         slm_pupil_coords_y = slm_pupil_coords[0]
+    #         slm_pupil_coords_x = slm_pupil_coords[1]
+    #         kx = None
+    #         ky = None
+    #     else: 
+    #         raise ValueError("Only full coordinate system is supported for pupil to k-space conversion.")
 
-            # convert pupil coordinates to k-space coordinates
-        return kx, ky    
-
+    #         # convert pupil coordinates to k-space coordinates
+    #     return kx, ky    
 
     
-
 class FilterPropagate(Processor):
     """
     Take a phase as input, build the field and filter it with the SLM filter.
@@ -64,6 +65,7 @@ class FilterPropagate(Processor):
         Compute the field at the SLM plane given the SLM phases.
         """
         # compute coordinates from source SLM
-        kx, ky = self._incident_field.pupil2kspace_coordinates()
+        x, y = self._incident_field._coordinates()
         # propagation...
-        return self.slm_filter(PhaseToField.read())
+
+        return self.slm_filter()
