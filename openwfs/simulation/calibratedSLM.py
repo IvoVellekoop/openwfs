8821eff8227d1768a064ee7b254cad009e7c0537
ArnonBAB <a.b.ab@utwente.nl>
2026-08-18 16:56:45 +0200

    Move filter processor to pwfs repo for further development

--- openwfs/simulation/calibratedSLM.py
+++ openwfs/simulation/calibratedSLM.py
@@ -30,29 +30,3 @@ class CalibratedSLM(SLM):
             raise ValueError("amplitude must have the same shape as the SLM shape.")
 
         self.physical_size = physical_size
-
-
-class FilterPropagate(Processor):
-    """
-    Computes 'Filter[slm.field]'.
-    """
-
-    def __init__(self, incident_field: CalibratedSLM, Microscope: MicroscopeOfTheFuture):
-        self.microscope = Microscope
-        self._incident_field = incident_field
-        super().__init__(incident_field)
-
-    def _fetch(self, incident_field: np.ndarray) -> np.ndarray:
-        """
-        Compute the field at the SLM plane given the SLM phases.
-        """
-        incident_field = set_extent(incident_field, self.microscope.wfs_module.settings.slm_physical_size)
-        incident_field = MicroscopeOfTheFuture.filter_hole(
-            incident_field,
-            self.microscope.wavelength,
-            self.microscope.wfs_module.settings.distance_mic_galvo,
-            self.microscope.wfs_module.settings.d_beam,
-            self.microscope.wfs_module.settings.d_mic_tube,
-        )
-        incident_field = set_extent(incident_field, self.microscope.wfs_module.settings.slm_physical_size)
-        return incident_field
