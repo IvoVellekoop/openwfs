f227e6d8d322197d3cb17b1cd73b8622e59a12fb
ArnonBAB <a.b.ab@utwente.nl>
2026-08-26 11:53:48 +0200

    Black

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -571,7 +571,7 @@ def test_mock_microscope_individual_components():
     """
     Test that the individual components of the microscope can be constructed and read out without exceptions being thrown.
     """
-        # test that the transform is applied correctly to the incident field in the microscope, and that the inverse transform can be used to cancel out the transform in the SLM
+    # test that the transform is applied correctly to the incident field in the microscope, and that the inverse transform can be used to cancel out the transform in the SLM
     glfw.init()
     if not glfw.get_monitors():
         pytest.skip("No monitors found", allow_module_level=True)
@@ -648,7 +648,7 @@ def test_mock_microscope_individual_components():
 
 @pytest.mark.parametrize("physical_size", [u.Quantity([2, 2], u.mm), u.Quantity([2, 4], u.mm), None])
 def test_transform_SLM_Aberration(physical_size):
-        # test that the transform is applied correctly to the incident field in the microscope, and that the inverse transform can be used to cancel out the transform in the SLM
+    # test that the transform is applied correctly to the incident field in the microscope, and that the inverse transform can be used to cancel out the transform in the SLM
     glfw.init()
     if not glfw.get_monitors():
         pytest.skip("No monitors found", allow_module_level=True)
