f68839c3329ae9fb46466e64ffb2025b3e39d455
DylanMMarques <d.m.marques@utwente.nl>
2026-09-23 16:04:56 +0200

    fix typo == to is

--- openwfs/simulation/slm.py
+++ openwfs/simulation/slm.py
@@ -254,7 +254,7 @@ class SLM(PhaseSLM, Actuator):
     @lookup_table.setter
     def lookup_table(self, value: Sequence[int] | None):
         max_value = 2**self.bit_depth - 1
-        if value == None:
+        if value is None:
             self._lookup_table = range(max_value + 1)
         elif np.min(value) < 0 or np.max(value) > max_value:
             raise ValueError(f"Lookup table values must be in the range [0, {max_value}]")
